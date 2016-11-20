78be3006a375cda2c2acdda391cf27f912410202
Marc Schlechter <marc1981@outlook.com>
2016-11-20 14:13:07 +0100

    Test situatie voor cachen gemaakt

---
+++ NancyFxTutorial.Web/Content/tijdController.js
@@ -0,0 +1,16 @@
+﻿app.controller("tijdController", function ($scope, $http) {
+  
+  $scope.tijdLaden = function () {
+    $http.get("/tijd").then(function (response) {
+      $scope.tijd = response.data;
+    });
+  };
+
+  $scope.tijdLaden();
+
+  $scope.vernieuwen = function () {
+    $scope.tijdLaden();
+  };
+
+
+});
---
+++ NancyFxTutorial.Web/Modules/TijdModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NancyFxTutorial.Web.Modules
+{
+  public class TijdModule : NancyModule
+  {
+    public TijdModule()
+    {
+      Get["/tijd"] = _ => DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
+    }
+  }
+}
