9f164e3a7169778ac4366ef2a0b7e1b22dec2df7
Marc Schlechter <marc@schlechter.nl>
2016-12-07 08:37:47 +0100

    LoggingService toegevoegd

---
+++ NancyFxTutorial.Web/Services/ILoggingService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NancyFxTutorial.Web.Services
+{
+  public interface ILoggingService
+  {
+    void LogMessage(string message);
+  }
+}
---
+++ NancyFxTutorial.Web/Services/LoggingService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NancyFxTutorial.Web.Services
+{
+  public class LoggingService : ILoggingService
+  {
+    IDbConnectionService DbConnectionService;
+
+    public LoggingService(IDbConnectionService dbConnectionService)
+    {
+      this.DbConnectionService = dbConnectionService;
+    }
+
+    public void LogMessage(string message)
+    {
+      var dbConnection = DbConnectionService.GetDbConnection();
+
+
+    }
+  }
+}
