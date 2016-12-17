57c8a3f39f95395a22100fa736ad8cb71f5f70ec
Marc Schlechter <marc@schlechter.nl>
2016-12-17 23:35:58 +0100

    Code opgeruimd

--- NancyFxTutorial.Web/CustomBootstrapper.cs
+++ NancyFxTutorial.Web/CustomBootstrapper.cs
@@ -23,15 +23,12 @@ namespace NancyFxTutorial.Web
       container.Update(builder =>
       {
         // SqlConnectionService registreren die gedurende een gehele request zal bestaan
-        builder
-          .Register(ctx => { return new SqlConnectionService(mainConnectionString); })
+        builder.Register(ctx => new SqlConnectionService(mainConnectionString))
           .As<IDbConnectionService>()
           .InstancePerRequest();
 
         // Registreer de WebTokenService
-        builder
-          .Register(ctx => { return new WebTokenService(issuerName, secretApiKey); })
-          .As<IWebTokenService>();
+        builder.Register(ctx => new WebTokenService(issuerName, secretApiKey)).As<IWebTokenService>();
 
         // Registreer de AuthenticationService
         builder.RegisterType<AuthenticationService>().As<IAuthenticationService>();
@@ -39,8 +36,6 @@ namespace NancyFxTutorial.Web
         // Registreer de LoggingService
         builder.RegisterType<LoggingService>().As<ILoggingService>();
       });
-
-      
     }
   }
 }
